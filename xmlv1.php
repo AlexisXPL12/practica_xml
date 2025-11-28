adaaadca9b25ee9a0fa7d7f3a092b7407b23c44b
AlexisXPL12 <valdiviatruciosalexisgabriel@gmail.com>
2025-11-28 10:25:36 -0500

    123

--- xmlv1.php
+++ xmlv1.php
@@ -1,4 +1,519 @@
 <?php
-$pe = ['DISEÑO Y PROGRAMACION WEB', 'EFERMERIA TÉCNICA', 'INDUSTRIAS DE ALIMENTOS Y BEBIDAS','MECATRONICA AUTOMOTRIZ', 'PRODUCCIÓN AGROPECUARIA'];
+$ies = [];
 
+//-------------------------- Diseño y Programación Web -------------------------------
+$udp1dpw = [
+    'Fundamentos de programación',
+    'Redes e internet',
+    'Análisis y diseño de sistemas',
+    'Introducción de base de datos',
+    'Arquitectura de computadoras',
+    'Comunicación oral',
+    'Aplicaciones en internet'
+];
+
+$udp2dpw = [
+    'Ofimática',
+    'Interpretación y producción de textos',
+    'Metodología de desarrollo de software',
+    'Programación orientada a objetos',
+    'Arquitectura de servidores web',
+    'Aplicaciones sistematizadas',
+    'Taller de base de datos'
+];
+
+$udp3dpw = [
+    'Administración de base de datos',
+    'Programación de aplicaciones web',
+    'Diseño de interfaces web',
+    'Pruebas de software',
+    'Inglés para la comunicación oral'
+];
+
+$udp4dpw = [
+    'Desarrollo de entornos web',
+    'Programación de soluciones web',
+    'Proyectos de software',
+    'Seguridad en aplicaciones web',
+    'Comprensión y redacción en inglés',
+    'Comportamiento ético'
+];
+
+$udp5dpw = [
+    'Programación de aplicaciones móviles',
+    'Marketing digital',
+    'Diseño de soluciones web',
+    'Gestión y administración de sitios web',
+    'Diagramación digital',
+    'Solución de problemas',
+    'Oportunidades de negocios'
+];
+
+$udp6dpw = [
+    'Plataforma de servicios web',
+    'Ilustración y gráfica digital',
+    'Administración de servidores web',
+    'Comercio electrónico',
+    'Plan de negocios'
+];
+
+//-------------------------- Enfermería Técnica -------------------------------
+$udp1et = [
+    'Salud comunitaria',
+    'Promoción de la salud',
+    'Educación para la salud',
+    'Asistencia en inmunizaciones',
+    'Seguridad y salud en el trabajo',
+    'Comunicación oral',
+    'Aplicaciones en internet'
+];
+
+$udp2et = [
+    'Epidemiología',
+    'Salud en desastres naturales',
+    'Prevención en salud pública',
+    'Anatomía y fisiología humana',
+    'Fundamentos de bioseguridad',
+    'Interpretación y producción de textos',
+    'Ofimática'
+];
+
+$udp3et = [
+    'Asistencia básica hospitalaria',
+    'Administración de medicamentos',
+    'Documentación en salud',
+    'Microbiología y muestras biológicas',
+    'Inglés para la comunicación oral',
+    'Comunicación efectiva en Quechua'
+];
+
+$udp4et = [
+    'Atención en urgencias y emergencias',
+    'Procedimientos invasivos y no invasivos',
+    'Bioestadística en salud',
+    'Dietas y nutrición',
+    'Comprensión y redacción en inglés',
+    'Medio ambiente y desarrollo sostenible'
+];
+
+$udp5et = [
+    'Salud materno neonatal',
+    'Atención del niño y adolescente',
+    'Atención al usuario quirúrgico',
+    'Atención del adulto y adulto mayor',
+    'Técnicas de resolución de conflictos',
+    'Comportamiento ético'
+];
+
+$udp6et = [
+    'Medicina alternativa',
+    'Fisioterapia y rehabilitación',
+    'Salud mental',
+    'Atención del paciente en estado crítico',
+    'Innovación tecnológica',
+    'Plan de negocios'
+];
+
+//-------------------------- Mecatrónica -------------------------------
+$udp1m = [
+    'Mantenimiento automotriz',
+    'Mecánica de taller y metrología automotriz',
+    'Mantenimiento preventivo de la suspensión, dirección y frenos',
+    'Mantenimiento preventivo del sistema eléctrico',
+    'Seguridad laboral',
+    'Comunicación oral',
+    'Aplicaciones en internet'
+];
+
+$udp2m = [
+    'Dibujo mecánico CAD automotriz',
+    'Hidráulica y neumática',
+    'Cálculos aplicados a los sistemas de suspensión, dirección y frenos',
+    'Mantenimiento y reparación del sistema de suspensión y dirección con asistencia electrónica',
+    'Mantenimiento del sistema de frenos con asistencia electrónica',
+    'Interpretación y producción de textos',
+    'Ofimática'
+];
+
+$udp3m = [
+    'Sistemas de luces',
+    'Sistema de carga y arranque',
+    'Sistema de encendido automotriz con asistencia electrónica',
+    'Sistema de implementos, seguridad y confort',
+    'Dibujo eléctrico y electrónico automotriz',
+    'Comportamiento ético',
+    'Inglés para la comunicación oral'
+];
+
+$udp4m = [
+    'Sistema de transmisión automática y transferencia',
+    'Mecanismo diferencial',
+    'Mecanismo de embrague y caja de velocidades manual',
+    'Comprensión y redacción en inglés',
+    'Estrategias para la solución de problemas'
+];
+
+$udp5m = [
+    'Motores de combustión interna Otto',
+    'Inyección electrónica Otto',
+    'Sistema CAN BUS y unidades de control electrónico',
+    'Cálculos aplicados al motor de combustión interna',
+    'Oportunidades de negocio',
+    'Fundamentos de innovación tecnológica'
+];
+
+$udp6m = [
+    'Motores de combustión interna Diesel con asistencia electrónica',
+    'Sistema de inyección Diesel con gestión electrónica',
+    'Afinamiento electrónico de MCI',
+    'Conversión a combustibles alternos GLP / GNV',
+    'Plan de negocios',
+    'Proyecto de innovación tecnológica'
+];
+
+//-------------------------- Industrias Alimentarias -------------------------------
+$udp1ia = [
+    'Fundamentos de la industria alimentaria',
+    'Microbiología general',
+    'Química general',
+    'Buenas prácticas de manipulación',
+    'Higiene y saneamiento industrial',
+    'Comunicación oral',
+    'Aplicaciones en internet'
+];
+
+$udp2ia = [
+    'Microbiología de alimentos',
+    'Química orgánica aplicada',
+    'Control de calidad de materias primas',
+    'Operaciones unitarias básicas',
+    'Ofimática',
+    'Interpretación y producción de textos',
+    'Seguridad alimentaria'
+];
+
+$udp3ia = [
+    'Tecnología de productos cárnicos',
+    'Tecnología de frutas y hortalizas',
+    'Tecnología de cereales y derivados',
+    'Análisis físico-químico de alimentos',
+    'Inglés para la comunicación oral'
+];
+
+$udp4ia = [
+    'Nutrición y valor nutritivo de alimentos',
+    'Envasado y conservación de alimentos',
+    'Gestión de procesos alimentarios',
+    'Comprensión y redacción en inglés',
+    'Comportamiento ético'
+];
+
+$udp5ia = [
+    'Formulación y desarrollo de nuevos productos',
+    'Control y aseguramiento de calidad',
+    'Gestión de plantas de alimentos',
+    'Análisis sensorial',
+    'Oportunidades de negocio',
+    'Solución de problemas'
+];
+
+$udp6ia = [
+    'Normas y regulaciones alimentarias',
+    'Auditoría en calidad alimentaria',
+    'Proyecto de innovación en alimentos',
+    'Plan de negocios alimentarios',
+    'Tecnologías emergentes en alimentos'
+];
+
+//-------------------------- Producción Agropecuaria -------------------------------
+$udp1pa = [
+    'Fundamentos de producción agropecuaria',
+    'Manejo de herramientas y maquinarias',
+    'Botánica y fisiología vegetal',
+    'Zootecnia general',
+    'Comunicación oral',
+    'Aplicaciones en internet',
+    'Higiene y seguridad en el trabajo agrícola'
+];
+
+$udp2pa = [
+    'Agronomía general',
+    'Manejo de suelos',
+    'Sanidad vegetal',
+    'Acuicultura básica',
+    'Ofimática',
+    'Interpretación y producción de textos',
+    'Reproducción animal'
+];
+
+$udp3pa = [
+    'Riego tecnificado',
+    'Producción de cultivos andinos',
+    'Manejo de pastos y forrajes',
+    'Producción de animales menores',
+    'Inglés para la comunicación oral'
+];
+
+$udp4pa = [
+    'Producción de frutales',
+    'Nutrición y alimentación animal',
+    'Gestión de unidades agropecuarias',
+    'Comprensión y redacción en inglés',
+    'Comportamiento ético'
+];
+
+$udp5pa = [
+    'Producción orgánica',
+    'Agroindustria rural',
+    'Manejo integrado de recursos naturales',
+    'Solución de problemas',
+    'Oportunidades de negocio',
+    'Sanidad e inocuidad agroalimentaria'
+];
+
+$udp6pa = [
+    'Comercialización agropecuaria',
+    'Gestión de emprendimientos rurales',
+    'Proyecto de innovación agropecuaria',
+    'Plan de negocios agropecuario',
+    'Agroexportación'
+];
+//-------------------------- PERIODOS DPW -------------------------------
+$p1dpw = [];
+$p1dpw['nombre'] = "I";
+$p1dpw['unidades_didacticas'] = $udp1dpw;
+
+$p2dpw = [];
+$p2dpw['nombre'] = "II";
+$p2dpw['unidades_didacticas'] = $udp2dpw;
+
+$p3dpw = [];
+$p3dpw['nombre'] = "III";
+$p3dpw['unidades_didacticas'] = $udp3dpw;
+
+$p4dpw = [];
+$p4dpw['nombre'] = "IV";
+$p4dpw['unidades_didacticas'] = $udp4dpw;
+
+$p5dpw = [];
+$p5dpw['nombre'] = "V";
+$p5dpw['unidades_didacticas'] = $udp5dpw;
+
+$p6dpw = [];
+$p6dpw['nombre'] = "VI";
+$p6dpw['unidades_didacticas'] = $udp6dpw;
+
+//-------------------------- PERIODOS ENFERMERIA TECNICA-------------------------------
+$p1et = [];
+$p1et['nombre'] = "I";
+$p1et['unidades_didacticas'] = $udp1et;
+
+$p2et = [];
+$p2et['nombre'] = "II";
+$p2et['unidades_didacticas'] = $udp2et;
+
+$p3et = [];
+$p3et['nombre'] = "III";
+$p3et['unidades_didacticas'] = $udp3et;
+
+$p4et = [];
+$p4et['nombre'] = "IV";
+$p4et['unidades_didacticas'] = $udp4et;
+
+$p5et = [];
+$p5et['nombre'] = "V";
+$p5et['unidades_didacticas'] = $udp5et;
+
+$p6et = [];
+$p6et['nombre'] = "VI";
+$p6et['unidades_didacticas'] = $udp6et;
+
+//-------------------------- PERIODOS MECATRONICA -------------------------------
+$p1m = [];
+$p1m['nombre'] = "I";
+$p1m['unidades_didacticas'] = $udp1m;
+
+$p2m = [];
+$p2m['nombre'] = "II";
+$p2m['unidades_didacticas'] = $udp2m;
+
+$p3m = [];
+$p3m['nombre'] = "III";
+$p3m['unidades_didacticas'] = $udp3m;
+
+$p4m = [];
+$p4m['nombre'] = "IV";
+$p4m['unidades_didacticas'] = $udp4m;
+
+$p5m = [];
+$p5m['nombre'] = "V";
+$p5m['unidades_didacticas'] = $udp5m;
+
+$p6m = [];
+$p6m['nombre'] = "VI";
+$p6m['unidades_didacticas'] = $udp6m;
+
+//-------------------------- PERIODOS INDUSTRIAS -------------------------------
+$p1ia = [];
+$p1ia['nombre'] = "I";
+$p1ia['unidades_didacticas'] = $udp1ia;
+
+$p2ia = [];
+$p2ia['nombre'] = "II";
+$p2ia['unidades_didacticas'] = $udp2ia;
+
+$p3ia = [];
+$p3ia['nombre'] = "III";
+$p3ia['unidades_didacticas'] = $udp3ia;
+
+$p4ia = [];
+$p4ia['nombre'] = "IV";
+$p4ia['unidades_didacticas'] = $udp4ia;
+
+$p5ia = [];
+$p5ia['nombre'] = "V";
+$p5ia['unidades_didacticas'] = $udp5ia;
+
+$p6ia = [];
+$p6ia['nombre'] = "VI";
+$p6ia['unidades_didacticas'] = $udp6ia;
+
+//-------------------------- PERIODOS AGROPECUARIA -------------------------------
+$p1pa = [];
+$p1pa['nombre'] = "I";
+$p1pa['unidades_didacticas'] = $udp1pa;
+
+$p2pa = [];
+$p2pa['nombre'] = "II";
+$p2pa['unidades_didacticas'] = $udp2pa;
+
+$p3pa = [];
+$p3pa['nombre'] = "III";
+$p3pa['unidades_didacticas'] = $udp3pa;
+
+$p4pa = [];
+$p4pa['nombre'] = "IV";
+$p4pa['unidades_didacticas'] = $udp4pa;
+
+$p5pa = [];
+$p5pa['nombre'] = "V";
+$p5pa['unidades_didacticas'] = $udp5pa;
+
+$p6pa = [];
+$p6pa['nombre'] = "VI";
+$p6pa['unidades_didacticas'] = $udp6pa;
+
+//-------------------------- MODULOS DPW -------------------------------
+
+$m1dpw = array();
+$m1dpw['nombre'] = "ANALISIS Y DISEÑO DE SISTEMAS WEB";
+$m1dpw['periodos'] = [$p1dpw, $p2dpw];
+
+$m2dpw = array();
+$m2dpw['nombre'] = "DESARROLLO DE APLICACIONES WEB";
+$m2dpw['periodos'] = [$p3dpw, $p4dpw];
+
+$m3dpw = array();
+$m3dpw['nombre'] = "DISEÑO DE SERVICIOS WEB";
+$m3dpw['periodos'] = [$p5dpw, $p6dpw];
+
+//-------------------------- MODULOS ET -------------------------------
+$m1et = array();
+$m1et['nombre'] = "ASISTENCIA EN PROMOCIÓN Y PREVENCIÓN DE LA SALUD";
+$m1et['periodos'] = [$p1et, $p2et];
+
+$m2et = array();
+$m2et['nombre'] = "ASISTENCIA EN LA ATENCIÓN BÁSICA DE LA SALUD";
+$m2et['periodos'] = [$p3et, $p4et];
+
+$m3et = array();
+$m3et['nombre'] = "ASISTENCIA EN LA ATENCIÓN INTEGRAL EN SALUD";
+$m3et['periodos'] = [$p5et, $p6et];
+
+//-------------------------- MODULOS M -------------------------------
+$m1m = array();
+$m1m['nombre'] = "MANTENIMIENTO DE VEHÍCULOS CONVENCIONALES Y CON ASISTENCIA ELECTRÓNICA";
+$m1m['periodos'] = [$p1m, $p2m];
+
+$m2m = array();
+$m2m['nombre'] = "MANTENIMIENTO DE SISTEMAS ELÉCTRICOS Y ELECTRÓNICOS AUTOMOTRICES";
+$m2m['periodos'] = [$p3m, $p4m];
+
+$m3m = array();
+$m3m['nombre'] = "MANTENIMIENTO DEL SISTEMA DE TRANSMISIÓN DE VEHÍCULOS CON ASISTENCIA ELECTRÓNICA";
+$m3m['periodos'] = [$p5m, $p6m];
+
+//-------------------------- MODULOS IA -------------------------------
+$m1ia = array();
+$m1ia['nombre'] = "PROCESAMIENTO PRIMARIO DE PRODUCTOS ALIMENTARIOS";
+$m1ia['periodos'] = [$p1ia, $p2ia];
+
+$m2ia = array();
+$m2ia['nombre'] = "TRANSFORMACIÓN DE PRODUCTOS ALIMENTARIOS";
+$m2ia['periodos'] = [$p3ia, $p4ia];
+
+$m3ia = array();
+$m3ia['nombre'] = "GESTIÓN Y DESARROLLO DE PRODUCTOS ALIMENTARIOS";
+$m3ia['periodos'] = [$p5ia, $p6ia];
+
+//-------------------------- MODULOS PA -------------------------------
+$m1pa = array();
+$m1pa['nombre'] = "MANEJO DE LA PRODUCCIÓN AGROPECUARIA";
+$m1pa['periodos'] = [$p1pa, $p2pa];
+
+$m2pa = array();
+$m2pa['nombre'] = "MEJORAMIENTO Y MANEJO INTEGRAL DE LA PRODUCCIÓN";
+$m2pa['periodos'] = [$p3pa, $p4pa];
+
+$m3pa = array();
+$m3pa['nombre'] = "GESTIÓN AGROPRODUCTIVA SOSTENIBLE";
+$m3pa['periodos'] = [$p5pa, $p6pa];
+
+//-------------------------- PROGRAMAS DE ESTUDIO -------------------------------
+
+$pe1 = array();
+$pe1['nombre'] = "DISEÑO Y PROGRAMACIÓN WEB";
+$pe1['modulos'] = [$m1dpw, $m2dpw, $m3dpw];
+
+$pe2 = array();
+$pe2['nombre'] = "ENFERMERÍA TÉCNICA";
+$pe2['modulos'] = [$m1et, $m2et, $m3et];
+
+$pe3 = array();
+$pe3['nombre'] = "MECATRÓNICA";
+$pe3['modulos'] = [$m1m, $m2m, $m3m];
+
+$pe4 = array();
+$pe4['nombre'] = "INDUSTRIAS ALIMENTARIAS";
+$pe4['modulos'] = [$m1ia, $m2ia, $m3ia];
+
+$pe5 = array();
+$pe5['nombre'] = "PRODUCCIÓN AGROPECUARIA";
+$pe5['modulos'] = [$m1pa, $m2pa, $m3pa];
+
+//--------------------------------- IES ---------------------------------------
+
+$ies['nombre'] = "IES Público HUANTA";
+$ies['programas_estudio'] = [$pe1, $pe2, $pe3, $pe4, $pe5];
+
+$xml = new DOMDocument('1.0','UTF-8');
+$xml->formatOutput = true;
+
+$et1 = $xml->createElement('ies');
+$xml-> appendChild($et1);
+
+
+$nombre_ies = $xml->createElement("nombre", $ies['nombre']);
+$programas_ies = $xml->createElement("programas_estudio");
+
+foreach ($ies["programas_estudio"] as $indice => $PEs) {
+    $num_pe = $xml->createElement("pe".$indice+1);
+    $nombre_pe = $xml->createElement("nombre", $PEs['nombre']);
+    $num_pe->appendChild($nombre_pe);
+    $programas_ies->appendChild($nombre_pe);
+}
+
+
+$archivo = "ies.xml";
+$xml->save($archivo);
 ?>
